faeb8fe55d4c5bc69f278d3743e2edc8220499e0
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-09-12 05:24:37 -0300

    I created a game container, this is simplifedgame loop and will be updated in the future

---
+++ src/main/java/com/vinis/vEngine/GameContainer.java
@@ -0,0 +1,104 @@
+package com.vinis.vEngine;
+
+import org.jetbrains.annotations.NotNull;
+
+public class GameContainer implements Runnable{
+
+    private @NotNull Thread thread;
+
+    private boolean running = false;
+
+    private final double UPDATE_CAP = 1.0 / 60.0;
+    public GameContainer() {
+
+    }
+
+    public void start() {
+
+        this.thread = new Thread(this);
+
+        this.thread.run();
+    }
+
+    public void stop() {
+
+        this.thread.stop();
+
+        this.running = false;
+    }
+
+    public void run() {
+
+        this.running = true;
+
+        double firstTime = 0;
+        double lastTime = System.nanoTime() / 1000000000.0;
+        double passedTime = 0;
+        double unprocessedTime = 0;
+
+        double frameTime = 0;
+        int frames = 0;
+        int fps = 0;
+
+        while (running) {
+
+            boolean render = false;
+
+            firstTime = System.nanoTime() / 1000000000.0;
+            passedTime = firstTime - lastTime;
+            lastTime = firstTime;
+
+            unprocessedTime = unprocessedTime + passedTime;
+
+            frameTime = frameTime + passedTime;
+
+            while (unprocessedTime >= UPDATE_CAP) {
+
+                unprocessedTime = unprocessedTime - UPDATE_CAP;
+
+                render = true;
+
+                //todo: update game
+
+                if (frameTime >= 1.0) {
+
+                    frameTime = 0;
+                    fps = frames;
+                    frames = 0;
+
+                    System.out.println( "FPS: " + fps);
+                }
+            }
+
+            if (render) {
+
+                frames = frames + 1;
+
+                //todo: render game
+            } else {
+
+                try {
+
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        dispose();
+    }
+
+    private void dispose() {
+
+        //todo: create a code of dispose
+    }
+
+    public static void main(String[] args) {
+
+        @NotNull GameContainer gc = new GameContainer();
+
+        gc.start();
+    }
+}
