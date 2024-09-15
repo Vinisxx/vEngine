d97fcb141549bab38feed33f32b6481d167b86c7
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-09-15 04:51:58 -0300

    I created the final class Location for substitute the params X and Y em some classes

---
+++ src/main/java/com/codes/vinis/utils/Location.java
@@ -0,0 +1,61 @@
+package com.codes.vinis.utils;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public final class Location {
+
+    private int x, y;
+
+    public Location(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+
+        return x;
+    }
+
+    public void setX(int x) {
+
+        this.x = x;
+    }
+
+    public int getY() {
+
+        return y;
+    }
+
+    public void setY(int y) {
+
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Location location = (Location) o;
+
+        return x == location.x && y == location.y;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public @NotNull String toString() {
+
+        return "{ \"X\": " + getX() + ", \"Y\": " + getY() + " }";
+    }
+}
