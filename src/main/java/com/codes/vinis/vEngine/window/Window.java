7b57702f398e8cc20d1782c3a37394be6164ae45
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-09-16 09:49:04 -0300

    I updated the window for the new a new model more accurate

--- src/main/java/com/codes/vinis/vEngine/window/Window.java
+++ src/main/java/com/codes/vinis/vEngine/window/Window.java
@@ -1,87 +1,109 @@
 package com.codes.vinis.vEngine.window;
 
-import com.codes.vinis.vEngine.core.Renderer;
 import com.codes.vinis.vEngine.utils.Dimension;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class Window {
 
     private final @NotNull JFrame FRAME;
 
-    private final @NotNull Dimension INITIAL_DIMENSION;
+    private final @NotNull BufferedImage IMAGE;
+
+    private final @NotNull BufferStrategy BUFFER_STRATEGY;
+
+    private final @NotNull Canvas CANVAS;
+
+    private final @NotNull Graphics GRAPHICS;
 
     private @NotNull Dimension dimension;
 
-    private @NotNull Renderer renderer;
+    private final float SCALE;
 
-    public Window(@NotNull String title, @NotNull Dimension dimension) {
-
-        this.FRAME = new JFrame(title);
-
-        this.INITIAL_DIMENSION = dimension;
+    public Window(@NotNull String title, @NotNull Dimension dimension, float scale) {
 
         this.dimension = dimension;
 
-        this.renderer = new Renderer(getInitialDimension());
+        this.SCALE = scale;
 
-        initialize();
-    }
+        this.IMAGE = new BufferedImage(getDimension().getWidth(), getDimension().getHeight(), BufferedImage.TYPE_INT_RGB);
 
-    private void initialize() {
+        this.CANVAS = new Canvas();
 
-        this.FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.FRAME.setSize(getInitialDimension().getWidth(), getInitialDimension().getHeight());
-        this.FRAME.setLayout(new BorderLayout());
+        @NotNull java.awt.Dimension size = new java.awt.Dimension((int) (dimension.getWidth() * getSCALE()), (int) (dimension.getHeight() * getSCALE()));
 
-        renderer.setPreferredSize(new java.awt.Dimension(INITIAL_DIMENSION.getWidth(), INITIAL_DIMENSION.getHeight()));
+        getCANVAS().setPreferredSize(size);
+        getCANVAS().setMaximumSize(size);
+        getCANVAS().setMinimumSize(size);
 
-        this.FRAME.add(renderer, BorderLayout.CENTER);
-        this.FRAME.pack();
-        this.FRAME.add(getRenderer());
-        this.FRAME.setLocationRelativeTo(null);
-        this.FRAME.setVisible(true);
+        this.FRAME = new JFrame(title);
+
+        getFRAME().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getFRAME().setLayout(new BorderLayout());
+        getFRAME().add(getCANVAS(), BorderLayout.CENTER);
+        getFRAME().pack();
+        getFRAME().setLocationRelativeTo(null);
+        getFRAME().setResizable(true);
+        getFRAME().setVisible(true);
+
+        getCANVAS().createBufferStrategy(2);
+
+        this.BUFFER_STRATEGY = getCANVAS().getBufferStrategy();
+
+        this.GRAPHICS = getBUFFER_STRATEGY().getDrawGraphics();
     }
 
     public void update() {
 
-        if (getFrame().getWidth() != getDimension().getWidth() || getFrame().getHeight() != getDimension().getHeight()) {
+        if (getFRAME().getWidth() != getDimension().getWidth() || getFRAME().getHeight() != getDimension().getHeight()) {
 
-            setDimension(new Dimension(getFrame().getWidth(), getFrame().getHeight()));
+            setDimension(new Dimension(getFRAME().getWidth(), getFRAME().getHeight()));
+
+            @NotNull java.awt.Dimension size = new java.awt.Dimension((int) (getDimension().getWidth() * getSCALE()), (int) (getDimension().getHeight() * getSCALE()));
+
+            getCANVAS().setPreferredSize(size);
+            getCANVAS().setMaximumSize(size);
+            getCANVAS().setMinimumSize(size);
         }
 
-        getRenderer().update();
+        getGRAPHICS().drawImage(getIMAGE(), 0, 0, getCANVAS().getWidth(), getCANVAS().getHeight(), null);
+
+        getBUFFER_STRATEGY().show();
     }
 
-    public @NotNull JFrame getFrame() {
-
+    public @NotNull JFrame getFRAME() {
         return FRAME;
     }
 
-    public @NotNull Dimension getInitialDimension() {
+    public @NotNull BufferedImage getIMAGE() {
+        return IMAGE;
+    }
 
-        return INITIAL_DIMENSION;
+    public @NotNull BufferStrategy getBUFFER_STRATEGY() {
+        return BUFFER_STRATEGY;
+    }
+
+    public @NotNull Canvas getCANVAS() {
+        return CANVAS;
+    }
+
+    public @NotNull Graphics getGRAPHICS() {
+        return GRAPHICS;
     }
 
     public @NotNull Dimension getDimension() {
-
         return dimension;
     }
 
     public void setDimension(@NotNull Dimension dimension) {
-
         this.dimension = dimension;
     }
 
-    public @NotNull Renderer getRenderer() {
-
-        return renderer;
-    }
-
-    public void setRenderer(@NotNull Renderer renderer) {
-
-        this.renderer = renderer;
+    public float getSCALE() {
+        return SCALE;
     }
 }
